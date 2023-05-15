86ac71858e733e7e9e6753146b076328e3b8423c
AhmedSohag <ahmedsohag0154@gmail.com>
2023-05-16 03:41:23 +0500

    Go to feature implemented

--- cypress/e2e/TestPlan.cy.ts
+++ cypress/e2e/TestPlan.cy.ts
@@ -1,9 +1,15 @@
+import { LandingPage } from "./LandingPage"
 import { LoginPage } from "./LoginPage"
 
 describe('template spec', () => {
   it('Visit Website and Login', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    
     let loginPage = new LoginPage()
     loginPage.loginUser()
+    let landingPage = new LandingPage()
+    landingPage.goToAdmin()
+
+    
   })
 })
